12158cff2aa483e744390f45c17b7f23147d5efd
griffinjoshs <leveloutusa@gmail.com>
2025-09-27 16:41:12 -0400

    typescript excercise

--- src/components/section/SectionTypes.ts
+++
@@ -1,7 +0,0 @@
-// src/components/section/SectionTypes.ts
-import type { CollectionKey } from 'astro:content';
-
-export interface SectionProps {
-  collection?: CollectionKey;
-  variant?: string;
-}

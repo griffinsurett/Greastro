1ab7446e10981be53ab1a7008a8308545c16dc7f
griffinjoshs <leveloutusa@gmail.com>
2025-09-27 20:33:38 -0400

    layout typesafe

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,48 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+
+// Load environment variables from .env files
+const env = loadEnv(
+  process.env.NODE_ENV || 'development', 
+  process.cwd(), 
+  ''
+);
+
+const siteUrl = `https://${env.PUBLIC_SITE_DOMAIN}`;
+console.log(`Site URL: ${siteUrl}`);
 
 export default defineConfig({
-  integrations: [mdx()],
+  site: siteUrl,
+  server: {
+    port: 9090,
+  },
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        '@': '/src',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (
+              id.includes('node_modules/react') ||
+              id.includes('node_modules/react-dom')
+            ) {
+              return 'react-vendor';
+            }
+          },
+        },
       },
     },
   },
+  integrations: [
+    mdx(),
+    react({
+      include: ['**/react/*', '**/components/**/*.jsx', '**/hooks/**/*.js']
+    }),
+  ],
 });

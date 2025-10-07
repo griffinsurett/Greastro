4cb1731467f1daaaca7373a2cc3f6e64f8f92811
griffinjoshs <leveloutusa@gmail.com>
2025-10-07 13:52:46 -0400

    before collection querying

--- astro.config.mjs
+++ astro.config.mjs
@@ -51,7 +51,7 @@ export default defineConfig({
   ],
   
   build: {
-    inlineStylesheets: 'always', // This tells Astro to inline CSS automatically
+    inlineStylesheets: 'always',
     split: true,
   },
   

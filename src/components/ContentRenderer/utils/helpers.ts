20a9d5d06b6fd4b18b0fbcb72fcdb20deeea5d25
griffinjoshs <leveloutusa@gmail.com>
2025-10-17 00:18:22 -0400

    section now content renderer

---
+++ src/components/ContentRenderer/utils/helpers.ts
@@ -0,0 +1,11 @@
+// src/components/ContentRenderer/utils/helpers.ts
+
+/**
+ * Check if collection CTA should be displayed
+ */
+export function shouldShowCollectionCTA(
+  collectionUrl?: string,
+  itemCount?: number
+): boolean {
+  return !!(collectionUrl && itemCount && itemCount > 0);
+}
